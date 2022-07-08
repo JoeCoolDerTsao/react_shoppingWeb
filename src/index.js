b01ab331ecda52725fc4d51ec865795a43effa1d
JoeTsao <zxc00211@gmail.com>
2022-07-08 13:30:42 +0800

    delete init file and add new file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import ReactDOM from 'react-dom'; //react 17用法
+import ReactDOM from 'react-dom/client' //react 18
+import Router from 'Router';
+import { ToastContainer } from 'react-toastify';//第三方通知套件：https://fkhadra.github.io/react-toastify/introduction
+import 'react-toastify/dist/ReactToastify.css';
+import 'css/app.scss';
+import 'css/style.scss';
+import 'commons/auth';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//ReactDOM.render(<Router />, document.getElementById("root")); //react 17用法
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div>
+    {/* 在全域這裡引用toast container 就可以在其他頁面用toast*/}
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+    <Router />
+  </div>
+); //react 18
+//參考：https://github.com/reactwg/react-18/discussions/5
